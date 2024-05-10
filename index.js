0105e519596cd1012ba2211e76a50e229e187ae1
Ruben Salazar <rubensalazar@rocketmail.com>
2024-05-09 21:06:30 -0500

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+const num1 = 31
+
+const num2 = 2
+
+const multiply = 31 * 2
+
+
+const random = 1
+
+const num3 = 32
+
+const num4 = 8
+
+const mod = 32 / 8
+
+const max = 20
